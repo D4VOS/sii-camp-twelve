f42a4ff066b519124dd25db9510604e9c6f7c3fe
dbryla <d4vos@icloud.com>
2022-04-26 20:09:16 +0200

    input actions wraapper

---
+++ src/main/java/helpers/web/wrappers/InputActions.java
@@ -0,0 +1,15 @@
+package helpers.web.wrappers;
+
+import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InputActions {
+    private static final Logger logger = LoggerFactory.getLogger(InputActions.class);
+
+    public static void performSendKeys(WebElement field, String text) {
+        logger.info(" <<<<< Entering: '" + text + "' into the field at: " + field.getLocation());
+        field.clear();
+        field.sendKeys(text);
+    }
+}
